f874994342e2e2993e64e0db54716ce5675404aa
Ingrid Sanuto <ingridsanutoaguiar1414@gmail.com>
2026-01-26 18:54:23 -0300

    utilizando o cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json()); // permite JSON no body
 routes(app); // chama o index.js das rotas
