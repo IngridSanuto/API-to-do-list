34edcd2c44866f78ba4ab22596596181896f2c24
Ingrid Sanuto <ingridsanutoaguiar1414@gmail.com>
2026-01-28 12:09:54 -0300

    adicionando .env

--- src/app.js
+++ src/app.js
@@ -3,7 +3,24 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
+
+app.options('', cors());
+
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
 
 app.use(express.json()); // permite JSON no body
 routes(app); // chama o index.js das rotas
